e2051b990ad7d0b8cf947b829b20cb7df3eb7e4b
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-18 00:07:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,8 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-  dwarves.each do |dwarf, index
+  dwarves.each do |dwarf, index |
+    
   
 end
 
